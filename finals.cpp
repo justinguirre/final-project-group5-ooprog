ae2070461c3719605442e82a23a6e0819de84056
Justin Guirre <justinchaseg@icloud.com>
2024-11-28 12:49:37 +0800

    added ParkInnLodge functions

--- finals.cpp
+++ finals.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <iomanip>
 using namespace std;
+
+bool intIsValid(string input) {
+    if (input.empty() || input[0] == '-') {
+        return false;
+    }
+
+    for (char c : input) {
+        if (!isdigit(c)) {
+            return false;
+        }
+    }
+
+    int num = stoi(input);
+    return num > 0;
+}
 
 //Class User
 class User {
@@ -98,14 +114,78 @@ class ParkInnLodge {
       int roomNo;
       int choice;
       cout << "Enter room number of the room to edit: ";
-      cin >> roomNo;
+      getline(cin, roomNo);
+      cin.clear();
+      cin.ignore(numeric_limits<streamsize>::max(), '\n');
 
       bool found = false;
       for (const auto& room: rooms) {
         if (room.getRoomNo() == roomNo) {
           found = true;
+          break;
         }
       }
+      if (found) {
+        cout << left;
+        cout << setw(15) << "NUMBER" << setw(20) << "TYPE" << setw(10) << "PRICE" << setw(15) "AVAILABLE?" << setw(15) << endl;
+        cout << "-----------------------------------------------------------------------------------" << endl;
+        for (const auto& room: rooms) {
+          if (room.getRoomNo() == roomNo) {
+            cout << setw(15) << room.getRoomNo() << setw(20) << room.getRoomType() << setw(10) << room.getRoomPrice() << setw(15) << room.roomIsAvailable() << setw(15) << endl;
+          }
+        }
+        int editChoice;
+        cout << "\nEdit Room:" << endl;
+        cout << "[1] Number" << endl;
+        cout << "[2] Type" << endl;
+        cout << "[3] Price" << endl;
+        cout << "[4] Availability" << endl;
+        cout << "[0] Return to Home" << endl;
+        cout << "INPUT: " << endl;
+        getline(cin, editChoice);
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        switch (editChoice) {
+          case 0:
+            // return to home
+            return;
+          case 1:
+            // edit room no
+            int newRoomNo;
+            do {
+              cout << "Enter new room number: ";
+              getline(cin, newRoomNo);
+              cin.clear();
+              cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+              bool roomNoExists = false;
+              for (const auto& iroom: rooms) {
+                if (iroom.getRoomNo() == newRoomNo) {
+                  roomNoExists = true;
+                  break;
+                }
+              }
+            } while ((!intIsValid(newRoomNo)) || (roomNoExists));
+
+            room.setRoomNo(newRoomNo);
+            break;
+          
+          case 2:
+            // edit room type
+            break;
+
+          case 3:
+            // edit room price
+            break;
+
+          case 4:
+            // edit room availability
+            break;
+
+        }
+      }
+
     }
 
     virtual void viewBookingHistory () const {
